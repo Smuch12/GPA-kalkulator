92bb91e19ae631379b68e4dcf2288eb05dcbaf8c
Smuch12 <95097400+Smuch12@users.noreply.github.com>
2024-01-25 00:31:54 +0100

    added data to table

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-import os
+import os, sqlite3
 
 from flask import (
     Flask,
@@ -15,4 +15,14 @@ app = Flask(
 
 @app.get("/")
 def index_html():
-    return render_template("index.html")
+    db_path = APP_PATH + '\db\Grades.db'
+
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM KARAKTERER')
+    rows = cur.fetchall()
+    conn.close()
+
+    return render_template("index.html", rows=rows)
